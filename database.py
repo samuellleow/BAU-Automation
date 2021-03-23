a204a149f70596c38f2de0276979d751af962d9b
samuellleow <samuel_leow@hotmail.com>
2021-03-23 11:57:07 +0800

    remove confidentials

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="127.0.0.1",
   user="root",
-  password="mrranmoru98",
+  password="",
   database="CaseNumber"
 )
 mycursor = mydb.cursor()
